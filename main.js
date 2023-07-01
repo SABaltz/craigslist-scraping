737639b1eee105e51ab4f70cef33441b02ab0999
sam <samuelabaltz@gmail.com>
2023-07-01 12:02:23 -0600

    Optimize Code

--- main.js
+++ main.js
@@ -1,66 +1,38 @@
 const cheerio = require("cheerio");
-// const pretty = require("pretty");
 const axios = require("axios");
 const fs = require('fs');
-const url = 'https://anchorage.craigslist.org/search/roo#search=1'
+const url = 'https://anchorage.craigslist.org/search/roo#search=1';
+const filePath = 'pids.txt';
 
 async function scrapeData() {
     try {
-        const {data} = await axios.get(url);
+        const { data } = await axios.get(url);
         const $ = cheerio.load(data);
-
         const listItems = $("ol li");
 
-        let count = 0
+        const fileContent = fs.readFileSync(filePath, 'utf-8');
+        const existingPids = new Set(fileContent.split('\n'));
 
-        const fileContent = fs.readFileSync('pids.txt', 'utf-8');
-        const lines = fileContent.split('\n');
-
-        let emptyFile
-        emptyFile = lines.length === 1;
-
-        let lineArray = []
-        const fileData = fs.readFileSync('pids.txt', 'UTF-8')
-        const fileLines = fileData.split(/\r?\n/)
-        fileLines.forEach(line => {
-            lineArray.push(line)
-        })
-
+        let newPids = [];
 
         listItems.each((index, el) => {
             if (index !== 0) {
-                let href = $(el).children('a').attr('href')
-                let post = String(href).match(/(\d+)\.html$/)[1]
-                count++
+                const href = $(el).children('a').attr('href');
+                const postId = href.match(/(\d+)\.html$/)?.[1];
 
-                if (emptyFile) {
-                    fs.appendFile('pids.txt', post + '\n', (err) => {
-                        if (err) {
-                            console.error(err);
-                        }
-                    });
-                } else {
-                    if (!lineArray.includes(post)){
-                        console.log(href)
-                        fs.appendFile('pids.txt', post + '\n', (err) => {
-                            if (err) {
-                                console.error(err);
-                            }
-                        });
-                    }
+                if (postId && !existingPids.has(postId)) {
+                    newPids.push(postId);
+                    console.log(href);
                 }
-
-
-
             }
-
         });
 
-
+        if (newPids.length > 0) {
+            fs.appendFileSync(filePath, newPids.join('\n') + '\n');
+        }
     } catch (err) {
         console.error(err);
     }
 }
 
 scrapeData();
-

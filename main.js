3eb0c09578f822272d92c8cdcf7c53644f342e3d
sam <samuelabaltz@gmail.com>
2023-07-01 11:16:50 -0600

    Get main tags

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 const cheerio = require("cheerio");
 const pretty = require("pretty");
+const axios = require("axios");
+
+const url = 'https://anchorage.craigslist.org/search/roo#search=1'
+
+async function scrapeData() {
+    try {
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+
+        const listItems = $("ol li");
+
+        listItems.each((index, el) => {
+            console.log($(el).text())
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+// Invoke the above function
+scrapeData();
